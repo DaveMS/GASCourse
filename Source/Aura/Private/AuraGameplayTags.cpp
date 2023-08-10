c5c472d18fcc85d0dde57a37f7bbf331c397f33e
Dave <david.martin.sutcliffe@gmail.com>
2023-08-10 17:04:49 +0100

    8-11

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,10 @@
+﻿#include "AuraGameplayTags.h"
+
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitialiseNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armour"), FString("Reduces damage taken, improves Block Chance"));
+}
---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * Singleton containing native Gameplay Tags.
+ *
+ */
+struct FAuraGameplayTags
+{
+public:
+
+ static const FAuraGameplayTags& Get() { return GameplayTags; };
+
+ static void InitialiseNativeGameplayTags();
+protected:
+
+private:
+
+ static FAuraGameplayTags GameplayTags;
+};
