9b2c5dcc58ce817276b85e4dba4f6732c2115533
Dave <david.martin.sutcliffe@gmail.com>
2023-08-11 21:22:48 +0100

    8-14

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright David Sutcliffe
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& Info : AttributeInformation)
+	{
+		if (Info.AttributeTag == AttributeTag)
+		{
+			return Info;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find info for attribute tag %s"), *AttributeTag.ToString());
+	}
+
+	return FAuraAttributeInfo();
+}
