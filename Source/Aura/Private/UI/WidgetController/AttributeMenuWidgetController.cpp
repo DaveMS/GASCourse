c1af40260c5187b791839f733ca62ef73cd8c262
Dave <david.martin.sutcliffe@gmail.com>
2023-08-11 22:06:21 +0100

    8-15

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright David Sutcliffe
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	Super::BroadcastInitialValues();
+}
---
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,21 @@
+﻿// Copyright David Sutcliffe
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void BindCallbacksToDependencies() override;
+	virtual void BroadcastInitialValues() override;
+};
