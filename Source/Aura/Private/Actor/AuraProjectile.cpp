0513bb85326a8558b10c870c9c5a6e6d7d500e5b
Dave <david.martin.sutcliffe@gmail.com>
2023-08-17 19:05:10 +0100

    9-13

---
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright David Sutcliffe
+
+
+#include "Actor/AuraProjectile.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+AAuraProjectile::AAuraProjectile()
+{
+
+	PrimaryActorTick.bCanEverTick = false;
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	SetRootComponent(Sphere);
+
+	MovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("Projectile movement");
+	MovementComponent->InitialSpeed = 550.f;
+	MovementComponent->MaxSpeed = 550.f;
+	MovementComponent->ProjectileGravityScale = 0.f;
+}
+
+void AAuraProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraProjectile::OnSphereOverlap);
+}
+
+void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
